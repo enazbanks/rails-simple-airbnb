a9545bb94f1c79a0c9fe72181fa4b12039f9561e
Enaz Banks <enazrbanks@gmail.com>
2022-11-16 08:56:43 +1100

    flat model controller and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Call this with 'rails db:seed' in command line
+
+puts "Creating flats..."
+flat1 = {name: "Dishoom Room", address: "7 Boundary St, London E2 7JE", description: "One bedroom" , price_per_night: 200, number_of_guests: 3}
+flat2 =  {name: "Pizza House", address: "56A Pepperoni St, London E1 6PQ", description: "Pineapple belongs on pizza"  , price_per_night: 455 , number_of_guests: 10}
+flat3 =  {name: "Burger House", address: "56A Shoreditch High St, London E1 6PQ", description: "All the piz", price_per_night: 50, number_of_guests: 10}
+flat4 =  {name: "Crack House", address: "56A Shoreditch High St, London E1 6PQ", description: "All the piz", price_per_night: 550, number_of_guests: 10}
+flat5 =  {name: "KFC House", address: "Fried Chicken St", description: "All the chicken", price_per_night: 30, number_of_guests: 4}
+
+[flat1, flat2, flat3, flat4, flat5].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
